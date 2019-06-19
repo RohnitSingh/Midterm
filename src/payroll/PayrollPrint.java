da891c3cab6dd05bbfc47ed3e15b99d20311e691
RohnitSingh <singrohn@sheridancollege.ca>
2019-06-19 15:21:01 -0400

    This is student demo programme

--- src/payroll/PayrollPrint.java
+++ src/payroll/PayrollPrint.java
@@ -11,20 +11,28 @@ import java.util.Scanner;
  * printing final salary including bonus
  * @author srinivsi
  */
-public class PayrollPrint 
+public class PayrollPrint
 {
 public static void main(String[] args) 
 {
 /* here assumed that 50 employee accounts can be created
  for the given bank */
-Payroll ac[]=new Payroll[50];
-Scanner sc=new Scanner(System.in);
-System.out.println("Please enter the employee name:");
-//add code
-System.out.println("Please enter the salary: salary <=3000");
-       
-System.out.println("The final Salary including bonus is: ");         
+    
+    Payroll ac[]=new Payroll[50];
+    Scanner sc=new Scanner(System.in);
+ 
+    for(int i = 0; i < ac.length; i++){
+        System.out.println("Please enter the employee name:");
+        String name = sc.nextLine();
+        double salary;
+        do {
+            System.out.println("Please enter the salary: salary <=3000");
+            salary = sc.nextDouble();
+        } while (salary > 3000);
+    System.out.println("The final Salary including bonus is: " + ac[i].calculation());
 
+       
+    }
 }
 
 }
